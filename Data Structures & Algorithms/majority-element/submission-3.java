3ef809756fb3b49c9735304f9aaf49d6c2e9909c
Coding Monkey <38658113+keertirajmalik@users.noreply.github.com>
2026-07-07 23:18:00 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> frequency = new HashMap();
+        int max = 0;
+        int sol = nums[0];
+
+        for (int i = 0; i < nums.length; i++) {
+            int val = nums[i];
+            if (frequency.containsKey(val)) {
+                int count = frequency.get(val) + 1;
+                frequency.put(val, count);
+
+                if (count > max) {
+                    max = count;
+                    sol = val;
+                }
+
+                if (max > (nums.length / 2)) {
+                    return val;
+                }
+            } else {
+                frequency.put(val, 1);
+            }
+        }
+
+        return sol;
+    }
+}
