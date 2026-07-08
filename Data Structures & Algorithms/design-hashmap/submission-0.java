9bf074a1b654213cef4bc25f4ca01b21bfa6b51d
Coding Monkey <38658113+keertirajmalik@users.noreply.github.com>
2026-07-08 23:43:24 +0530

    Add: design-hashmap - submission-0

---
+++ Data Structures & Algorithms/design-hashmap/submission-0.java
@@ -0,0 +1,66 @@
+class Node {
+    int key;
+    int value;
+    Node next;
+
+    Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class MyHashMap {
+    Node[] map;
+    public MyHashMap() {
+        map = new Node[1000];
+        for (int i = 0; i < map.length; i++) {
+            map[i] = new Node(0, 0);
+        }
+    }
+
+    public void put(int key, int value) {
+        int index = key % map.length;
+        Node cur = map[index];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next.value = value;
+                return;
+            }
+            cur = cur.next;
+        }
+        cur.next = new Node(key, value);
+    }
+
+    public int get(int key) {
+        int index = key % map.length;
+        Node cur = map[index];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                return cur.next.value;
+            }
+            cur = cur.next;
+        }
+        return -1;
+    }
+
+    public void remove(int key) {
+        int index = key % map.length;
+        Node cur = map[index];
+        while (cur.next != null) {
+            if (cur.next.key == key) {
+                cur.next = cur.next.next;
+                return;
+            }
+            cur = cur.next;
+        }
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
