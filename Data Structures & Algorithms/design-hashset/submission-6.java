4b13c9737bcff1ca8cd1747bfb3167957e6bf098
Coding Monkey <38658113+keertirajmalik@users.noreply.github.com>
2026-07-08 23:09:10 +0530

    Add: design-hashset - submission-6

---
+++ Data Structures & Algorithms/design-hashset/submission-6.java
@@ -0,0 +1,49 @@
+class MyHashSet {
+    int[] set;
+    int index;
+
+    public MyHashSet() {
+        set = new int[100000];
+        index = 0;
+    }
+
+    public void add(int key) {
+        if (contains(key)) {
+            return;
+        }
+        set[index] = key;
+        index += 1;
+    }
+
+    public void remove(int key) {
+        if(!contains(key)){
+            return;
+        }
+        int k = 0;
+        for (int i = 0; i < index; i++) {
+            if (set[i] == key) {
+                continue;
+            }
+            set[k] = set[i];
+            k += 1;
+        }
+        index -= 1;
+    }
+
+    public boolean contains(int key) {
+        for (int i = 0; i < index; i++) {
+            if (set[i] == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
