8d4a847ed489988e0ff8a0b6d990b678974bb7c8
Coding Monkey <38658113+keertirajmalik@users.noreply.github.com>
2026-07-11 22:32:29 +0530

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String encode(List<String> strs) {
+        String encode = "";
+        for (String str : strs) {
+            encode = encode + "%" + str.length() + "%" + str;
+        }
+        return encode;
+    }
+
+    public List<String> decode(String str) {
+        List<String> decode = new ArrayList();
+
+        while (str.length() > 0) {
+            String count = str.split("%", 3)[1];
+            int length = Integer.parseInt(count);
+            decode.add(str.substring(count.length() + 2, count.length() + 2 + length));
+            // System.out.println(str.substring(count.length() + 2 + length, str.length()));
+            str = str.substring(count.length() + 2 + length, str.length());
+        }
+        return decode;
+    }
+}
